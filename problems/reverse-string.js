5951e3eed27fd29638d20e3f7b7c7a34018f9b63
Tanzil <mtanzilislam1997@gmail.com>
2022-10-19 10:29:44 +0100

    completed reverse str

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,12 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string !== 'string') {
+    throw new Error;
+  }
+
+  return string
+    .split('')
+    .reverse()
+    .join('');
+
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,17 @@
 // Your code here
+
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const reverseString = require('../problems/reverse-string.js');
+
+describe(`reverseString`, function() {
+    it('should reverse the string', function() {
+        expect(reverseString(`fun`)).to.equal('nuf')             //entered string in test function and set to equal nuf
+    });
+
+    it(`should throw an error when argument is not a string`, function() {
+        expect(() => reverseString(0987)).throw()  /// entered nonstring in the test function
+    })
+})
